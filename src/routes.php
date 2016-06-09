e7aee1cbbcfc0def0b0754926a56e8b9351809a5
Adam Berecz <adam@webaxis.hu>
2016-06-09 23:45:56 +0200

    fixes

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,7 @@ Route::group(['middleware' => 'web'], function () {
 	Route::post('/wx/form/validateform', 'Waxis\Form\FormController@validateform');
 	Route::post('/wx/form/cloning', 'Waxis\Form\FormController@cloning');
 	Route::post('/wx/form/suggest', 'Waxis\Form\FormController@suggest');
-	Route::post('/wx/form/prefetch', 'Waxis\Form\FormController@prefetch');
+	Route::any('/wx/form/prefetch', 'Waxis\Form\FormController@prefetch');
 	Route::post('/wx/form/loaddata', 'Waxis\Form\FormController@loaddata');
 	Route::post('/wx/form/validationremote', 'Waxis\Form\FormController@validationremote');
 	Route::post('/wx/form/upload', 'Waxis\Form\FormController@upload');
